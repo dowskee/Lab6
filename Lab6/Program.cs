6d474e04c2a4813040f422dc1de1f7f64a525be3
dowskee <31831031+dowskee@users.noreply.github.com>
2017-09-29 14:45:48 -0400

    Added loop

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -10,6 +10,36 @@ namespace Lab6
     {
         static void Main(string[] args)
         {
+            bool TranslateAgain = true;
+            while (TranslateAgain == true)
+            {
+                //application prompts user for a word
+                //application translates the text to Pig Latin and displays it on the console.
+                //application asks user if they want to translate another word.
+
+                //if Index [0] is a vowel, add + "way" to end of Length
+                //if Index [0] is a consonant, take consonants up until the first vowel in the word, move it to the end
+                //of the length, then add "ay". 
+
+                //use Indexof String and Sub String types
+                int line;
+
+                Console.WriteLine("Welcome to the Pig Latin Translator! Oink Oink!");
+                Console.WriteLine("Please enter a line you would like to translate:");
+
+                line = int.Parse(Console.ReadLine());
+
+
+            }
+            string Translate;
+            Console.WriteLine("Would you like to translate another word? (Y/N)");
+            Translate = Console.ReadLine();
+            if (Translate == "N" || Translate == "No" || Translate == "no" || Translate == "n")
+                //needs to work with string.TranslateLower/Upper.
+            {
+            TranslateAgain = false;
+            Console.WriteLine("See ya! Don't eat bacon!");
+            }
         }
     }
 }
